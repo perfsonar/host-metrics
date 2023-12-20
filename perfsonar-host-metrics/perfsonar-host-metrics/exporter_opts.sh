d3e32c79a5aeea60404a23364ef6d72d58ab0fe6
Andy Lake <andy@es.net>
2023-12-20 02:16:33 +0000

    Updating node_exporter config to include useful sysctl values. Also simplified management of those options.

--- perfsonar-host-metrics/perfsonar-host-metrics/exporter_opts.sh
+++ perfsonar-host-metrics/perfsonar-host-metrics/exporter_opts.sh
@@ -2,11 +2,30 @@
 
 TARGET_FILE="/etc/default/node_exporter"
 EDIT_MARKER='#Edited by PS_HOST_METRICS'
-NODE_EXPORTER_OPTS='PS_NODE_EXPORTER_OPTS=""'
 grep -q PS_HOST_METRICS ${TARGET_FILE}
 if [ $? -eq 1 ]; then
     TEMPFILE=$(mktemp)
-    echo ${EDIT_MARKER} | cat - ${TARGET_FILE} > ${TEMPFILE}
-    sed -E 's/^NODE_EXPORTER_OPTS="(.*)"/NODE_EXPORTER_OPTS="\1 --collector.systemd --collector.systemd.unit-include=^(elmond|grafana-server|httpd|logstash|node_exporter|opensearch-dashboards|opensearch|owamp|(perfsonar-.+)|postgresql|(pscheduler-.+)|(psconfig-.+))\.service"/' -i ${TEMPFILE}
+    echo ${EDIT_MARKER} > ${TEMPFILE}
+    OPTS="--collector.sysctl"
+    OPTS="${OPTS} --collector.sysctl.include=net.core.rmem_max"
+    OPTS="${OPTS} --collector.sysctl.include=net.core.rmem_default"
+    OPTS="${OPTS} --collector.sysctl.include=net.core.wmem_max"
+    OPTS="${OPTS} --collector.sysctl.include=net.core.wmem_default"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.tcp_rmem:min,default,max"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.tcp_wmem:min,default,max"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.tcp_no_metrics_save"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.tcp_mtu_probing"
+    OPTS="${OPTS} --collector.sysctl.include-info=net.core.default_qdisc"
+    OPTS="${OPTS} --collector.sysctl.include-info=net.ipv4.tcp_congestion_control"
+    OPTS="${OPTS} --collector.sysctl.include-info=net.ipv4.tcp_available_congestion_control"
+    OPTS="${OPTS} --collector.sysctl.include-info=net.ipv4.tcp_allowed_congestion_control"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.conf.all.arp_ignore"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.conf.all.arp_announce"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.conf.default.arp_filter"
+    OPTS="${OPTS} --collector.sysctl.include=net.ipv4.conf.all.arp_filter"
+    OPTS="${OPTS} --collector.sysctl.include=net.core.netdev_max_backlog"
+    OPTS="${OPTS} --collector.systemd"
+    OPTS="${OPTS} --collector.systemd.unit-include=^(elmond|grafana-server|httpd|logstash|node_exporter|opensearch-dashboards|opensearch|owamp|(perfsonar-.+)|postgresql|(pscheduler-.+)|(psconfig-.+))\.service"
+    echo "NODE_EXPORTER_OPTS=\"${OPTS}\"" >> ${TEMPFILE}
     mv ${TEMPFILE} ${TARGET_FILE}
 fi 
